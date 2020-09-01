d26101e46a17c682b589e7ef30681eec62fa8dc2
TOGBE Fanick Arthis <fiacremax@gmail.com>
2020-08-31 22:05:09 -0400

    update configfile

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ docker stop demo
 docker build -t webapp .
 docker system prune -f
 docker rmi golang:alpine
-docker rmi alpine:latest
-#docker rmi nginx:alpine
+docker rmi node:stretch
+docker rmi nginx:latest
 docker run -d --name demo -p 80:80 webapp
